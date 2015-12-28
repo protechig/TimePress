03359754960974130959e790b672dfe438ee8dc4
Zach Russell <zach@protechig.com>
2015-12-27 21:04:47 -0500

    inital commit

---
+++ trellopress.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: TrelloPress
+ * Plugin URI: https://protechig.com/trellopress
+ * Description: Time Tracking in Trello
+ * Version: 0.1
+ * Author: Zachary Russell
+ * Author URI: http://zachrussell.net
+ * License: GPL2
+ */
